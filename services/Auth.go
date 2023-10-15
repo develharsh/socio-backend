ee85d8811b63015efeac7ae4a86bc98f1758067c
harshvsinghme <hvsreal.223@gmail.com>
2023-10-15 19:11:17 +0530

    made signup api active

--- services/Auth.go
+++ services/Auth.go
@@ -2,13 +2,15 @@ package services
 
 import (
 	"context"
-	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 	"github.com/harshvsinghme/socio-backend.git/dbUtils"
 	globals "github.com/harshvsinghme/socio-backend.git/global"
 	"github.com/harshvsinghme/socio-backend.git/models"
+	"github.com/harshvsinghme/socio-backend.git/utils"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
@@ -26,24 +28,59 @@ func (service AuthService) Signup(ctx *gin.Context) {
 	var err error
 	var accessToken string
 	var firstInsert *mongo.InsertOneResult
+	var validation []string
+	var userId primitive.ObjectID
+	var result bson.M
 
 	if err = ctx.BindJSON(&reqBody); err != nil {
-		ctx.JSON(http.StatusBadRequest, gin.H{"message": "Invalid request body"})
+		ctx.JSON(http.StatusBadRequest, gin.H{"message": "Failed to save info", "errors": []string{"Invalid request body"}})
+		return
+	}
+
+	if reqBody.Name == "" {
+		validation = append(validation, "Name can't be empty")
+	}
+	if reqBody.Email == "" {
+		validation = append(validation, "Email can't be empty")
+	}
+	if reqBody.Password == "" {
+		validation = append(validation, "Password can't be empty")
+	} else {
+		var hashed []byte
+		hashed, err = utils.HashPassword(reqBody.Password)
+		if err != nil {
+			validation = append(validation, "Password is invalid")
+		}
+		reqBody.Password = string(hashed)
+	}
+
+	if len(validation) > 0 {
+		ctx.JSON(http.StatusBadRequest, gin.H{"message": "Invalid details", "errors": validation})
+		return
+	}
+
+	err = dbUtils.MongoClient.Database(globals.SECRETS.MONGO_DATABASE).Collection("users").FindOne(context.TODO(), bson.M{"email": reqBody.Email}).Decode(&result)
+	if err == nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"message": "Email already in use", "errors": []string{"Try another email or remove this from existing account"}})
 		return
 	}
 
 	firstInsert, err = dbUtils.MongoClient.Database(globals.SECRETS.MONGO_DATABASE).Collection("users").InsertOne(context.TODO(), reqBody)
 	if err != nil {
-		ctx.JSON(http.StatusInternalServerError, gin.H{"message": "Could not save user", "error": err.Error()})
+		ctx.JSON(http.StatusInternalServerError, gin.H{"message": "Could not save user", "errors": []string{err.Error()}})
 		return
 	}
-	fmt.Println(firstInsert.InsertedID)
-	_, err = dbUtils.MongoClient.Database(globals.SECRETS.MONGO_DATABASE).Collection("userdetails").InsertOne(context.TODO(), models.UserDetail{})
+	userId = firstInsert.InsertedID.(primitive.ObjectID)
 	if err != nil {
-		ctx.JSON(http.StatusInternalServerError, gin.H{"message": "Could not save user details", "error": err.Error()})
+		ctx.JSON(http.StatusInternalServerError, gin.H{"message": "Could not save user details", "errors": []string{err.Error()}})
 		return
 	}
-	ctx.JSON(http.StatusOK, gin.H{"message": "Registered Successfully", "accessToken": accessToken})
+	_, err = dbUtils.MongoClient.Database(globals.SECRETS.MONGO_DATABASE).Collection("userdetails").InsertOne(context.TODO(), models.UserDetail{UserId: userId})
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"message": "Could not save user details", "errors": []string{err.Error()}})
+		return
+	}
+	ctx.JSON(http.StatusOK, gin.H{"message": "Registered Successfully", "accessToken": accessToken, "errors": []string{}})
 }
 
 func (service AuthService) Login(ctx *gin.Context) {
