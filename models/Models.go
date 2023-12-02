69836f888d19de7cb8bd3b8a024923b59babb682
harshvsinghme <hvsreal.223@gmail.com>
2023-12-02 14:55:48 +0530

    added readme and post, like, comment schema

--- models/Models.go
+++ models/Models.go
@@ -21,3 +21,29 @@ type UserDetail struct {
 	Following int32              `bson:"following" json:"following"`
 	Followers int32              `bson:"followers" json:"followers"`
 }
+
+type Post struct {
+	DocID       primitive.ObjectID `bson:"_id,omitempty" json:"_id,omitempty"`
+	Title       string             `bson:"title" json:"title"`
+	Content     string             `bson:"content" json:"content"`
+	PostedBy    primitive.ObjectID `bson:"postedBy" json:"postedBy"`
+	Likes       int32              `bson:"likes" json:"likes"`
+	Comments    int32              `bson:"comments" json:"comments"`
+	IsPublished bool               `bson:"isPublished" json:"isPublished"` //is public?
+	CreatedAt   int64              `bson:"created_at" json:"created_at"`
+	UpdatedAt   int64              `bson:"updated_at" json:"updated_at"`
+}
+
+type Like struct {
+	DocID     primitive.ObjectID `bson:"_id,omitempty" json:"_id,omitempty"`
+	User      primitive.ObjectID `bson:"user" json:"user"`
+	CreatedAt int64              `bson:"created_at" json:"created_at"`
+}
+
+type Comment struct {
+	DocID     primitive.ObjectID `bson:"_id,omitempty" json:"_id,omitempty"`
+	User      primitive.ObjectID `bson:"user" json:"user"`
+	Content   string             `bson:"content" json:"content"`
+	CreatedAt int64              `bson:"created_at" json:"created_at"`
+	UpdatedAt int64              `bson:"updated_at" json:"updated_at"`
+}
